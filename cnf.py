c4b778f90a4c6270acf95dfe72ec47c0f21fa822
Emmanuel Bandres <35436932+ebandres@users.noreply.github.com>
2021-07-17 19:45:02 -0400

    store keys/values instead of math

--- cnf.py
+++ cnf.py
@@ -1,4 +1,17 @@
 import sys, json, datetime
+import pickle
+
+# Global variables to store all values for later retrieval during decoding
+value_dict = {}
+last_value = 1
+        
+def save_obj(obj, name):
+    with open('obj/'+ name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+def load_obj(name):
+    with open('obj/' + name + '.pkl', 'rb') as f:
+        return pickle.load(f)
 
 if __name__ == '__main__':
 
@@ -11,8 +24,16 @@ if __name__ == '__main__':
 
     def var(d, h, i, j):
         assert(0<= i and i <= N and 0<= j and j <=N)
-        value = f"{d}00{h}00{i}00{j}"
-        return value
+        #value = f"{d}00{h}00{i}00{j}"
+        #return value
+        global last_value
+
+        if f"{d}{h}{i}{j}" not in value_dict:
+            value_dict[f"{d}{h}{i}{j}"] = last_value
+            last_value += 1
+            return last_value - 1
+
+        return value_dict[f"{d}{h}{i}{j}"]
 
     clauses = []
     
@@ -36,13 +57,12 @@ if __name__ == '__main__':
                     for h in range(1, dur_hour + 1):
                         count += 1
     
-    print(count)
     # Los participantes deben jugar al menos una vez con cada uno de los otros participantes
     for i in range(1, N + 1): #Participante que juega como local
         for j in range(1, N + 1): #Participante que juega como visitante
             if i != j:
                 for d in range(1, dur_date + 1):
-                    temp += [var(d,h,i,j) for h in range(1, dur_hour + 1)]
+                    temp += [f"{var(d,h,i,j)}" for h in range(1, dur_hour + 1)]
 
                 clauses.append(temp)
                 temp = []
@@ -58,42 +78,44 @@ if __name__ == '__main__':
                                 if w != d or k != h:
                                     clauses.append([f"-{var(d,h,i,j)}", f"-{var(w,k,i,j)}"])
 
-    # # Un participante puede jugar a lo sumo una vez por dia
-    # for d in range(1, dur_date + 1):
-    #     for i in range(1, N + 1):
-    #         for j in range(1, N + 1):
-    #             if i != j:
-    #                 for h in range(1, dur_hour + 1):
-    #                     for w in range(1, N + 1):
-    #                         if w != i and w != j:
-    #                             for k in range(1, dur_hour + 1):
-    #                                 clauses.append([-var(d,h,i,j), -var(d,k,i,w)])
+    # Un participante puede jugar a lo sumo una vez por dia
+    for d in range(1, dur_date + 1):
+        for i in range(1, N + 1):
+            for j in range(1, N + 1):
+                if i != j:
+                    for h in range(1, dur_hour + 1):
+                        for w in range(1, N + 1):
+                            if w != i and w != j:
+                                for k in range(1, dur_hour + 1):
+                                    clauses.append([f"{-var(d,h,i,j)}", f"{-var(d,k,i,w)}"])
 
-    # # Dos juegos no pueden ocurrir al mismo tiempo
-    # for d in range(1, dur_date + 1):
-    #     for h in range(1, dur_hour + 1):
-    #         for i in range(1, N + 1):
-    #             for j in range(1, N + 1):
-    #                 if i != j:
-    #                     for w in range(1, N + 1):
-    #                         for k in range(1, N + 1):
-    #                             if (w != i or k != j) and w != k:
-    #                                 clauses.append([-var(d,h,i,j), -var(d,h,w,k)])
+    # Dos juegos no pueden ocurrir al mismo tiempo
+    for d in range(1, dur_date + 1):
+        for h in range(1, dur_hour + 1):
+            for i in range(1, N + 1):
+                for j in range(1, N + 1):
+                    if i != j:
+                        for w in range(1, N + 1):
+                            for k in range(1, N + 1):
+                                if (w != i or k != j) and w != k:
+                                    clauses.append([f"{-var(d,h,i,j)}", f"{-var(d,h,w,k)}"])
 
-    # # Un jugador no puede jugar ni de local ni de visitante dos dias consecutivos
-    # for d in range(1, dur_date):
-    #     for i in range(1, N + 1):
-    #         for j in range(1, N + 1):
-    #             if i != j:
-    #                 for h in range(1, dur_hour + 1):
-    #                     for w in range(1, N + 1):
-    #                         if w != i and w != j:
-    #                             for k in range(1, dur_hour + 1):
-    #                                 clauses.append([-var(d,h,i,j), -var(d+1,k,i,w)])
+    # Un jugador no puede jugar ni de local ni de visitante dos dias consecutivos
+    for d in range(1, dur_date):
+        for i in range(1, N + 1):
+            for j in range(1, N + 1):
+                if i != j:
+                    for h in range(1, dur_hour + 1):
+                        for w in range(1, N + 1):
+                            if w != i and w != j:
+                                for k in range(1, dur_hour + 1):
+                                    clauses.append([f"{-var(d,h,i,j)}", f"{-var(d+1,k,i,w)}"])
 
-    
+    print("Saving value dictionary in obj/keys.pkl")
+    save_obj(value_dict, "keys")
 
-    cnf = open("cnf.txt", "a")
+    print("Writing cnf.txt ...")
+    cnf = open("cnf.txt", "w")
     separator = " "
 
     cnf.write(f"p cnf {count} {len(clauses)} \n")
@@ -103,6 +125,7 @@ if __name__ == '__main__':
         cnf.write(separator.join(cls) + "\n")
 
     cnf.close()
+    print("Done")
 
 
 
