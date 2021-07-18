3284cdcfa4ca583b00d4a1aee42747527aca9a16
Emmanuel Bandres <35436932+ebandres@users.noreply.github.com>
2021-07-18 16:17:03 -0400

    removed obj dir

--- cnf.py
+++ cnf.py
@@ -6,7 +6,7 @@ value_dict = {}
 last_value = 1
         
 def save_obj(obj, name):
-    with open('obj/'+ name + '.pkl', 'wb') as f:
+    with open(name + '.pkl', 'wb') as f:
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
 def load_obj(name):
